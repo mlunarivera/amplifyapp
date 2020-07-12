fc326f77d45467c92923033f018b6b41985ad306
Martin Luna <martinluna@iMac-de-Martin.local>
2020-07-11 20:33:26 -0500

    added credentials

--- src/index.js
+++ src/index.js
@@ -4,9 +4,18 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import Amplify from 'aws-amplify';
-import config from './aws-exports.js';
+import config from './aws-exports';
 
-Amplify.configure(config);
+const awsmobile = {
+  "aws_project_region": "us-east-1",
+  "aws_cognito_identity_pool_id": "us-east-1:6f6ef6c4-148a-4a60-a688-dc5a58367626",
+  "aws_cognito_region": "us-east-1",
+  "aws_user_pools_id": "us-east-1_UCwhaHneG",
+  "aws_user_pools_web_client_id": "2prs0r6cnja2ennf03gb3c84gq",
+  "oauth": {}
+};
+
+Amplify.configure(awsmobile);
 
 
 ReactDOM.render(
