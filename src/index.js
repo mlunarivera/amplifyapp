dd7f2f264cff337b8340caf57cf46b7f2ef54b0a
Martin Luna <martinluna@iMac-de-Martin.local>
2020-07-12 19:50:28 -0500

    added auth

--- src/index.js
+++ src/index.js
@@ -7,11 +7,17 @@ import Amplify from 'aws-amplify';
 
 const awsmobile = {
   "aws_project_region": "us-east-1",
-  "aws_cognito_identity_pool_id": "us-east-1:6f6ef6c4-148a-4a60-a688-dc5a58367626",
+  "aws_cognito_identity_pool_id": "us-east-1:bb3dfd86-fbaf-4053-a095-a98661c66835",
   "aws_cognito_region": "us-east-1",
   "aws_user_pools_id": "us-east-1_UCwhaHneG",
   "aws_user_pools_web_client_id": "2prs0r6cnja2ennf03gb3c84gq",
-  "oauth": {}
+  "oauth": {},
+  "aws_appsync_graphqlEndpoint": "https://4xkqsduwrzg4vftnfzwwfjyymy.appsync-api.us-east-1.amazonaws.com/graphql",
+  "aws_appsync_region": "us-east-1",
+  "aws_appsync_authenticationType": "API_KEY",
+  "aws_appsync_apiKey": "da2-oen6uyjowfgerdur3orbrlbeu4",
+  "aws_user_files_s3_bucket": "synerginotesapp163808-dev",
+  "aws_user_files_s3_bucket_region": "us-east-1"
 };
 
 Amplify.configure(awsmobile);
